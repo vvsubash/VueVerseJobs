8ad31e14add17962007b9ee3de607160758a7882
vvsubashshareplay <venkat@shareplay.tv>
2026-06-15 11:01:35 +0530

    rollback

--- vuejobs-india/nuxt.config.ts
+++ vuejobs-india/nuxt.config.ts
@@ -1,14 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  compatibilityDate: "2025-07-15",
+  runtimeConfig: {
+    dbDriver: '',
+    dbHost: '',
+    dbPort: '',
+    dbUser: '',
+    dbPassword: '',
+    dbDatabase: '',
+    databaseUrl: '',
   },
-})
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["@iconify/vue", "@vue/devtools-kit", "@vue/devtools-core"],
+    },
+  },
+});
